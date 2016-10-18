b414bb7615fdb5bdd878a0fd894e0da631975158
lingwu-git <772171952@qq.com>
2016-10-18 10:30:12 +0800

    格式问题
    格式问题

--- src/com/lingwu/socket/ShortSocketServer.java
+++ src/com/lingwu/socket/ShortSocketServer.java
@@ -11,44 +11,47 @@ import java.net.UnknownHostException;
 
 /**
  * 短连接服务
+ * 
  * @author lingwu
  */
 public class ShortSocketServer {
 
 	private static final int port = 12345;
 	private static final String ip = "127.0.0.1";
-	
+
 	public static void main(String[] args) {
 		switch (Integer.parseInt(args[0])) {
-			case 1:
-				server();
-				break;
-			case 2:
-				client();
-				break;
-			default:
-				break;
+		case 1:
+			server();
+			break;
+		case 2:
+			client();
+			break;
+		default:
+			break;
 		}
 	}
 
 	private final static void client() {
 		try {
 			Socket socket = new Socket(ip, port);
-			//读取服务器端数据    
-            DataInputStream input = new DataInputStream(socket.getInputStream());
-            //向服务器端发送数据    
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            System.out.print("请输入: \t");    
-            String str = new BufferedReader(new InputStreamReader(System.in)).readLine();
-            out.writeUTF(str);
-            
-            String ret = input.readUTF();
-            System.out.println("服务器端返回过来的是: " + ret);
-              
-            out.close();  
-            input.close();
-            socket.close();
-            System.out.println("客户端关闭当前与服务器的链接" + System.currentTimeMillis());
+			// 读取服务器端数据
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+			// 向服务器端发送数据
+			DataOutputStream out = new DataOutputStream(
+					socket.getOutputStream());
+			System.out.print("请输入: \t");
+			String str = new BufferedReader(new InputStreamReader(System.in))
+					.readLine();
+			out.writeUTF(str);
+
+			String ret = input.readUTF();
+			System.out.println("服务器端返回过来的是: " + ret);
+
+			out.close();
+			input.close();
+			socket.close();
+			System.out.println("客户端关闭当前与服务器的链接" + System.currentTimeMillis());
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
@@ -82,36 +85,40 @@ public class ShortSocketServer {
 
 /**
  * 每个会话处理线程
+ * 
  * @author lingwu
  */
 class HandlerThread extends Thread {
-	
+
 	Socket socket;
-	
+
 	public HandlerThread(Socket s) {
 		socket = s;
 	}
-	
+
 	public void run() {
 		try {
 			System.out.println("连接成功" + socket.getInetAddress() + ":" + socket.getPort());
-			// 读取客户端数据    
-            DataInputStream input = new DataInputStream(socket.getInputStream());
-            String clientInputStr = input.readUTF();//这里要注意和客户端输出流的写方法对应,否则会抛 EOFException  
-            // 处理客户端数据    
-            System.out.println("客户端发过来的内容:" + clientInputStr);
+			// 读取客户端数据
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+			String clientInputStr = input.readUTF();// 这里要注意和客户端输出流的写方法对应,否则会抛
+													// EOFException
+			// 处理客户端数据
+			System.out.println("客户端发过来的内容:" + clientInputStr);
 
-            // 向客户端回复信息    
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            System.out.print("请输入:\t");
-            // 发送键盘输入的一行
-            String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
-            out.writeUTF(s);
-              
-            out.close();
-            input.close();
-            socket.close();
-            System.out.println("服务器关闭当前与客户端的链接" + System.currentTimeMillis());
+			// 向客户端回复信息
+			DataOutputStream out = new DataOutputStream(
+					socket.getOutputStream());
+			System.out.print("请输入:\t");
+			// 发送键盘输入的一行
+			String s = new BufferedReader(new InputStreamReader(System.in))
+					.readLine();
+			out.writeUTF(s);
+
+			out.close();
+			input.close();
+			socket.close();
+			System.out.println("服务器关闭当前与客户端的链接" + System.currentTimeMillis());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
